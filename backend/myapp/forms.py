2c49672428ebb468dac01414eba27f01d567a759
tings17 <tingsun0517@gmail.com>
2025-02-26 18:52:41 +0800

    book title etc

--- backend/myapp/forms.py
+++ backend/myapp/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Annotation
 
 class AnnotationForm(forms.ModelForm):
+    #a form class that inherits from Django's ModelForm (automatically generate from fields based on the fields in the model)
     class Meta:
+        #nested Meta class that provides metadata about the form
         model = Annotation
-        fields = ("image",)
+        fields = ["book_name", "image"]
